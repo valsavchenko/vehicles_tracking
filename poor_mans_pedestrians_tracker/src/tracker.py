35e2df05a66d913686fdbd8e9744694a4f84ad13
Valentin Savchenko <valentine.savchenko@gmail.com>
2024-08-14 22:27:43 +0300

    Prevent tracking from major deviations
    Watch out for incoming objects

--- poor_mans_pedestrians_tracker/src/tracker.py
+++ poor_mans_pedestrians_tracker/src/tracker.py
@@ -21,8 +21,14 @@ class _Track:
     def __iou(rect_a, rect_b):
         """
         """
-        rect_i = rect_a & rect_b
-        iou = rect_i.area() / float(rect_a.area() + rect_b.area() - rect_i.area())
+        i_l = max(rect_a[0], rect_b[0])
+        i_t = max(rect_a[1], rect_b[1])
+        i_r = min(rect_a[0] + rect_a[2], rect_b[0] + rect_b[2])
+        i_b = min(rect_a[1] + rect_a[3], rect_b[1] + rect_b[3])
+        rect_i = i_l, i_t, i_r - i_l, i_b - i_t
+
+        area = lambda r: r[2] * r[3]
+        iou = area(rect_i) / float(area(rect_a) + area(rect_b) - area(rect_i))
         return iou
 
     @timer(label='Track creation')
@@ -96,14 +102,18 @@ class Tracker:
         """
         self.__detector = detector
 
+        self.__frames_seen = 0
         self.__tracking_engine_creator = self.__get_tracking_engine_creator(strategy=settings['strategy'])
         self.__iou_threshold = settings['iou_threshold']
+        self.__re_detect_every = settings['re_detect_every']
         self.__tracks = []
 
     @timer(label='Frame analysis')
     def track(self, frame):
         """
         """
+        self.__frames_seen += 1
+
         # Identify tracks that couldn't be extended natively
         done_inds = []
         for ti, t in enumerate(self.__tracks):
@@ -114,7 +124,9 @@ class Tracker:
             done_inds.append(ti)
 
         # Wrap up, if there are some tracks and they were extended natively
-        if not done_inds and self.__tracks:
+        all_tracks_were_extended = not done_inds and self.__tracks
+        force_re_detection = 0 == self.__frames_seen % self.__re_detect_every
+        if all_tracks_were_extended and not force_re_detection:
             return [], []
 
         # Dismiss tracks failed to extended natively
